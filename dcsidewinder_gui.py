1becb6f3633fe354591ecf04c20eb1db502d1edb
Mirabai Knight <mkk@stenoknight.com>
2010-05-27 22:45:27 +0800

    DigitalCAT sidewinder_gui uploaded (sorry for forgetting before)

---
+++ dcsidewinder_gui.py
@@ -0,0 +1,24 @@
+import tktest
+import re
+import time
+import os
+from dCATDict import exportDic
+
+tktest.KeyEater(exportDic, 'dCAT').mainloop() 
+translationFileRead = open("log.txt", "r")
+translationFileWrite = open("write.txt", "w")
+text = translationFileRead.read()
+text = re.sub(r' {\^([a-zA-Z ]*)} ', lambda m: '%s ' % (m.group(1),), text)
+text = re.sub(r' {([a-zA-Z ]*)\^} ', lambda m: ' %s' % (m.group(1),), text)
+text = re.sub(r'{([a-zA-Z])([a-zA-Z]*)\^} ', lambda m: '%s' % (m.group(1).upper() + m.group(2),), text)
+text = re.sub(r' {\.} ([a-z])', lambda m: '. %s' % (m.group(1).upper(),), text)
+text = re.sub(r' {\?} ([a-z])', lambda m: '? %s' % (m.group(1).upper(),), text)
+text = re.sub(r' {\!} ([a-z])', lambda m: '! %s' % (m.group(1).upper(),), text)
+text = re.sub(r' {\,}', ',', text)
+text = re.sub(r' {\.}', '.', text)
+text = re.sub(r' {\?}', '?', text)
+text = re.sub(r' {\!}', '!', text)
+text = re.sub(r'^([a-z])', lambda m: '%s' % (m.group(1).upper()), text, 1)
+translationFileWrite.write(text)
+translationFileWrite.close()
+os.rename('write.txt','plover' + time.strftime('%m%d%y%H%M%S') + '.txt')
